5ae3eb2e8d3ddd511a14c1f0ec3a017f71d5c63e
Brandon Grenier <brandon.john.grenier@gmail.com>
2017-01-27 10:26:46 +1100

    Additional tests.

--- multivariate-linear-regression/src/test/java/ai/brandon/mlr/model/SupervisedTrainingSetTest.java
+++ multivariate-linear-regression/src/test/java/ai/brandon/mlr/model/SupervisedTrainingSetTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+@SuppressWarnings("unchecked")
 public class SupervisedTrainingSetTest {
 
     @Test
@@ -51,7 +52,7 @@ public class SupervisedTrainingSetTest {
     }
 
     @Test
-    public void shouldBeAbleToAddAdditionalTrainingInstancesToAnExistingTrainingSet() {
+    public void shouldBeAbleToAddAnAdditionalTrainingInstanceToAnExistingTrainingSet() {
         List<SupervisedTrainingInstance<Integer>> instances = new ArrayList<SupervisedTrainingInstance<Integer>>();
         instances.add(new SupervisedTrainingInstance<Integer>(20, 1, 1, 1, 1));
         instances.add(new SupervisedTrainingInstance<Integer>(25, 2, 2, 2, 2));
@@ -63,6 +64,66 @@ public class SupervisedTrainingSetTest {
         set.add(new SupervisedTrainingInstance<Integer>(60, 1, 1, 1, 1));
         set.add(new SupervisedTrainingInstance<Integer>(70, 1, 1, 1, 1));
         assertThat(set.getInstances().size()).isEqualTo(5);
+    }
+
+    @Test
+    public void shouldBeAbleToAddAnAdditionalListTrainingInstanceToAnExistingTrainingSet() {
+        List<SupervisedTrainingInstance<Integer>> instances1 = new ArrayList<SupervisedTrainingInstance<Integer>>();
+        instances1.add(new SupervisedTrainingInstance<Integer>(20, 1, 1, 1, 1));
+        instances1.add(new SupervisedTrainingInstance<Integer>(25, 2, 2, 2, 2));
+        instances1.add(new SupervisedTrainingInstance<Integer>(33, 3, 3, 3, 3));
+
+        SupervisedTrainingSet<Integer> set = new SupervisedTrainingSet<Integer>(4, instances1);
+        assertThat(set.getInstances().size()).isEqualTo(3);
+
+        List<SupervisedTrainingInstance<Integer>> instances2 = new ArrayList<SupervisedTrainingInstance<Integer>>();
+        instances2.add(new SupervisedTrainingInstance<Integer>(20, 1, 1, 1, 1));
+        instances2.add(new SupervisedTrainingInstance<Integer>(25, 2, 2, 2, 2));
+        instances2.add(new SupervisedTrainingInstance<Integer>(33, 3, 3, 3, 3));
+
+        set.add(instances2);
+        assertThat(set.getInstances().size()).isEqualTo(6);
+    }
+
+    @Test
+    public void shouldBeAbleToAddAdditionalVarArgTrainingInstancesToAnExistingTrainingSet() {
+        List<SupervisedTrainingInstance<Integer>> instances1 = new ArrayList<SupervisedTrainingInstance<Integer>>();
+        instances1.add(new SupervisedTrainingInstance<Integer>(20, 1, 1, 1, 1));
+        instances1.add(new SupervisedTrainingInstance<Integer>(25, 2, 2, 2, 2));
+        instances1.add(new SupervisedTrainingInstance<Integer>(33, 3, 3, 3, 3));
+
+        SupervisedTrainingSet<Integer> set = new SupervisedTrainingSet<Integer>(4, instances1);
+        assertThat(set.getInstances().size()).isEqualTo(3);
+
+        List<SupervisedTrainingInstance<Integer>> instances2 = new ArrayList<SupervisedTrainingInstance<Integer>>();
+        instances2.add(new SupervisedTrainingInstance<Integer>(20, 1, 1, 1, 1));
+        instances2.add(new SupervisedTrainingInstance<Integer>(25, 2, 2, 2, 2));
+        instances2.add(new SupervisedTrainingInstance<Integer>(33, 3, 3, 3, 3));
+
+        set.add(instances2.get(0), instances2.get(1), instances2.get(2));
+        assertThat(set.getInstances().size()).isEqualTo(6);
+    }
+
+    @Test
+    public void shouldBeAbleToReturnTheSupervisedTrainingInstanceAtTheSpecifiedIndex() {
+        List<SupervisedTrainingInstance<Integer>> instances = new ArrayList<SupervisedTrainingInstance<Integer>>();
+        instances.add(new SupervisedTrainingInstance<Integer>(20, 1, 1, 1, 1));
+        instances.add(new SupervisedTrainingInstance<Integer>(25, 2, 2, 2, 2));
+        instances.add(new SupervisedTrainingInstance<Integer>(33, 3, 3, 3, 3));
+
+        SupervisedTrainingSet<Integer> set = new SupervisedTrainingSet<Integer>(4, instances);
+        assertThat(set.instanceAt(0)).isEqualTo(instances.get(0));
+    }
+
+    @Test
+    public void shouldBeAbleToReturnTheIndexOfTheSpecifiedInstance() {
+        List<SupervisedTrainingInstance<Integer>> instances = new ArrayList<SupervisedTrainingInstance<Integer>>();
+        instances.add(new SupervisedTrainingInstance<Integer>(20, 1, 1, 1, 1));
+        instances.add(new SupervisedTrainingInstance<Integer>(25, 2, 2, 2, 2));
+        instances.add(new SupervisedTrainingInstance<Integer>(33, 3, 3, 3, 3));
+
+        SupervisedTrainingSet<Integer> set = new SupervisedTrainingSet<Integer>(4, instances);
+        assertThat(set.indexOf(instances.get(1))).isEqualTo(1);
     }
 
 }
