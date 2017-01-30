8ea9dfb048f3f843303138a84faf395541bb683c
Brandon Grenier <brandon.john.grenier@gmail.com>
2017-01-30 22:06:58 +1100

    update

--- multivariate-linear-regression/src/test/java/ai/brandon/mlr/functions/NormalisationFunctionTest.java
+++ multivariate-linear-regression/src/test/java/ai/brandon/mlr/functions/NormalisationFunctionTest.java
@@ -11,19 +11,19 @@ public class NormalisationFunctionTest {
     @Test
     public void shouldBeAbleToNormaliseAnInput() {
         NormalisationFunction function = new NormalisationFunction(new BigDecimal("5"), new BigDecimal("2"));
-        assertThat(function.normalise(5)).isEqualTo(new BigDecimal("0"));
-        assertThat(function.normalise(6)).isEqualTo(new BigDecimal("0.5"));
-        assertThat(function.normalise(4)).isEqualTo(new BigDecimal("-0.5"));
-        assertThat(function.normalise(0)).isEqualTo(new BigDecimal("-2.5"));
+        assertThat(function.normalise(5)).isEqualTo(new BigDecimal("0.0000000000"));
+        assertThat(function.normalise(6)).isEqualTo(new BigDecimal("0.5000000000"));
+        assertThat(function.normalise(4)).isEqualTo(new BigDecimal("-0.5000000000"));
+        assertThat(function.normalise(0)).isEqualTo(new BigDecimal("-2.5000000000"));
     }
 
     @Test
     public void shouldDefaultToARangeOfOneIfTheRangeIsZero() {
         NormalisationFunction function = new NormalisationFunction(new BigDecimal("5"), new BigDecimal("0"));
-        assertThat(function.normalise(5)).isEqualTo(new BigDecimal("0"));
-        assertThat(function.normalise(6)).isEqualTo(new BigDecimal("1"));
-        assertThat(function.normalise(4)).isEqualTo(new BigDecimal("-1"));
-        assertThat(function.normalise(0)).isEqualTo(new BigDecimal("-5"));
+        assertThat(function.normalise(5)).isEqualTo(new BigDecimal("0.0000000000"));
+        assertThat(function.normalise(6)).isEqualTo(new BigDecimal("1.0000000000"));
+        assertThat(function.normalise(4)).isEqualTo(new BigDecimal("-1.0000000000"));
+        assertThat(function.normalise(0)).isEqualTo(new BigDecimal("-5.0000000000"));
     }
 
     @Test(expected = IllegalArgumentException.class)
